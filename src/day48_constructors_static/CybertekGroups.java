7c58281452034e742809bd9801a812fcb5574124
Zahir-Demir <melekceylin@hotmail.com>
2021-05-29 15:04:11 -0400

    added group object in CybertekGroups.java.java

---
+++ src/day48_constructors_static/CybertekGroups.java
@@ -0,0 +1,32 @@
+package day48_constructors_static;
+
+import java.util.Arrays;
+
+public class CybertekGroups {
+    public static void main(String[] args) {
+        //dependency injection. Group object creation depends on String name
+        Group group1 = new Group("CyberBug");
+        //print size of members
+        System.out.println(group1.getMembers().size());
+        group1.addMember("Berk");
+        group1.addMember("Pavel");
+        group1.addMember("Elvin");
+        group1.addMember("Ali");
+        group1.addMember("Parvin");
+        group1.addMember("Rasim");
+        group1.addMember("Maris");
+        System.out.println(group1.getMembers().size());
+        System.out.println(group1.toString());
+
+        Group group2 = new Group("CyberCats");
+        group2.setMembers(Arrays.asList("Stephen","Wakshum","Akrem","Bulent","Andrea","Andrei"));
+        //print all members. not toString()
+        System.out.println("group2 members = "+ group2.getMembers());
+        //use if statement to check if Akrem is in group2
+        if(group2.getMembers().contains("Akrem")){
+            System.out.println("Akrem is member of group2");
+        }else{
+            System.out.println("Akrem is not  in group2");
+        }
+    }
+}
---
+++ src/day48_constructors_static/Group.java
@@ -0,0 +1,47 @@
+package day48_constructors_static;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group {
+    private String name;
+    private List<String> members;
+
+    //constructor that accepts a name
+    public Group(String name) {
+        this.name = name;
+        members = new ArrayList<>();
+    }
+
+    public void addMember(String newMember) {
+        members.add(newMember);
+    }
+
+    public void removeMember(String memberName) {
+        members.remove(memberName);
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "name='" + name + '\'' +
+                ", members=" + members +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<String> members) {
+        this.members = members;
+    }
+}
