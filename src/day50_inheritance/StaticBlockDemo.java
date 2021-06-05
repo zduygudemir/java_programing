1b3a95b2c4127731bf5984c18da160a4ddc61b12
Zahir-Demir <melekceylin@hotmail.com>
2021-06-05 11:59:33 -0400

    added carModel

--- src/day50_inheritance/StaticBlockDemo.java
+++ src/day50_inheritance/StaticBlockDemo.java
@@ -1,14 +1,94 @@
 package day50_inheritance;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StaticBlockDemo {
     static int num;
+    static List<String> carModels;
     static {
         System.out.println("static initializer block");
         num=10;
+        carModels = new ArrayList<>();
+        carModels.addAll(Arrays.asList("Abarth",
+                "Alfa Romeo",
+                "Aston Martin",
+                "Audi",
+                "Bentley",
+                "BMW",
+                "Bugatti",
+                "Cadillac",
+                "Chevrolet",
+                "Chrysler",
+                "Citroën",
+                "Dacia",
+                "Daewoo",
+                "Daihatsu",
+                "Dodge",
+                "Donkervoort",
+                "DS",
+                "Ferrari",
+                "Fiat",
+                "Fisker",
+                "Ford",
+                "Honda",
+                "Hummer",
+                "Hyundai",
+                "Infiniti",
+                "Iveco",
+                "Jaguar",
+                "Jeep",
+                "Kia",
+                "KTM",
+                "Lada",
+                "Lamborghini",
+                "Lancia",
+                "Land Rover",
+                "Landwind",
+                "Lexus",
+                "Lotus",
+                "Maserati",
+                "Maybach",
+                "Mazda",
+                "McLaren",
+                "Mercedes-Benz",
+                "MG",
+                "Mini",
+                "Mitsubishi",
+                "Morgan",
+                "Nissan",
+                "Opel",
+                "Peugeot",
+                "Porsche",
+                "Renault",
+                "Rolls-Royce",
+                "Rover",
+                "Saab",
+                "Seat",
+                "Skoda",
+                "Smart",
+                "SsangYong",
+                "Subaru",
+                "Suzuki",
+                "Tesla",
+                "Toyota",
+                "Volkswagen",
+                "Volvo"));
+    }
+
+    {//initializer block -> runs each time object created,before constructor
+        System.out.println("Initializer block");
 
     }
-    public StaticBlockDemo(){
+
+    public StaticBlockDemo() {
         System.out.println("Constructor method");
-        num+=5;
+        num += 5;
     }
+
+    //other methods that use the carModels arraylist
+
+
 }
+
